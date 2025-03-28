22a0395833eadcb21a5c55c7283296116cbc72d0
alima <tamannaalima@gmail.com>
2025-03-28 11:32:10 +0000

    create data base

--- includes/config.php
+++ includes/config.php
@@ -3,6 +3,20 @@ session_start();
 // Error reporting (disable in production)
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+$db = 'booklets';
+$host = 'localhost';
+$user = 'root';
+$pass = 'root';
+$pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+
+try {
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec('SET NAMES "utf8"');
+
+} catch (Exception $e) {
+    echo 'Could not connect to the database.';
+    exit;
+}
 ?>
 
 
