2894e2071ffad498a045299860f2b5089c41fc9d
alima <tamannaalima@gmail.com>
2025-03-28 11:20:48 +0000

    cosmetic change: centre allignment

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php include 'includes/config.php'; ?>
 <?php include 'includes/header.php'; ?>
 <main>
-    <h1>Welcome to My BookStore</h1>
-    <p> A small independent bookstore that is expanding its operations to include online sales</p>
+    <section class="content">
+        <h1>Welcome to My BookStore</h1>
+        <p> A small independent bookstore that is expanding its operations to include online sales</p>
+    </section>
 </main>
 <?php include 'includes/footer.php'; ?>
