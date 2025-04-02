ef44cf57171b1293625e199e118f6c65b7c91e16
alima <tamannaalima@gmail.com>
2025-04-02 18:19:18 +0100

    created login page

---
+++ login.php
@@ -0,0 +1,7 @@
+<?php include 'includes/config.php'; ?>
+<?php include 'includes/header.php'; ?>
+<main>
+    <h1>Login At My BookStore</h1>
+    <p> Login to view all of our book available here </p>
+</main>
+<?php include 'includes/footer.php'; ?>
