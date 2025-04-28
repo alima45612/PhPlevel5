81d3624b6be2b2efba5661f05d529d7e03e17132
alima <tamannaalima@gmail.com>
2025-04-28 22:01:33 +0100

    admin management page

---
+++ admin_management.php
@@ -0,0 +1,18 @@
+<?php include 'includes/config.php'; ?>
+<?php include 'includes/header.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Management</title>
+</head>
+<body>
+
+    <h1>Admin Management</h1>
+
+    <p><a href="admin_view_book.php">See All Books</a></p>
+    <p><a href="admin_add_book.php">Add New Book</a></p>
+
+</body>
+</html>
+
+<?php include 'includes/footer.php'; ?>
